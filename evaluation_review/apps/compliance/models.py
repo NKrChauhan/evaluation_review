7ed64cfb999fdcfd0bddc19be2eda6be3816583b
Nitish Kumar <nitishkumarchauhan1996@gmail.com>
2024-04-10 18:58:24 +0530

    perf(compliance): add created by for guidelines

--- evaluation_review/apps/compliance/models.py
+++ evaluation_review/apps/compliance/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Guideline(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    # Null = True is used since there is no authentication being set up yet
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
