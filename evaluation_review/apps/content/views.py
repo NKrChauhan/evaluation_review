1c796e8ae0c93bb853a78afaf576f84e0c9b9489
Nitish Kumar <nitishkumarchauhan1996@gmail.com>
2024-04-10 18:41:33 +0530

    faet(content): add reveiw api for status check for content

--- evaluation_review/apps/content/views.py
+++ evaluation_review/apps/content/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+
+from apps.review.services.review import ReviewService
 from .permissions import AuthorPermission
 from .models import Content
 from .serializers import ContentSerializer
@@ -14,6 +16,7 @@ class ContentUploadView(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(uploaded_by=self.request.user)
 
+
 class ContentUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
@@ -21,3 +24,17 @@ class ContentUpdateView(generics.RetrieveUpdateAPIView):
 
     def perform_update(self, serializer):
         serializer.save(uploaded_by=self.request.user)
+
+
+class ContentReviewStatusView(generics.RetrieveAPIView):
+    queryset = Content.objects.all()
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        review_details = ReviewService.get_review_details(instance)
+        
+        review_status = {}
+        for detail in review_details:
+            review_status[detail.guideline.title] = detail.passed
+        
+        return Response(review_status)
