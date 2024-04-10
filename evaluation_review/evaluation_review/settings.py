1681e3c80ca9c578fc766e94d2dff564d9f459db
Nitish Kumar <nitishkumarchauhan1996@gmail.com>
2024-04-10 05:54:03 +0530

    build: add apps templates to the project

--- evaluation_review/evaluation_review/settings.py
+++ evaluation_review/evaluation_review/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Custom app
+    'apps.compliance',
+    'apps.review',
+    'apps.content',
 ]
 
 MIDDLEWARE = [
